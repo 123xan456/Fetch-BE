f74505c025c581d2170d7ba26e36212a6ccda6c8
123xan456 <lxiaan27@gmail.com>
2024-12-08 20:09:43 -0600

    first commit

---
+++ client.py
@@ -0,0 +1,100 @@
+import requests
+from datetime import datetime, timezone
+
+port = 8000
+host = "localhost"
+baseurl = "http://" + host + ":" + str(port)
+
+def prompt():
+    try:
+        print()
+        print(">> Enter a command:")
+        print("   0 => end")
+        print("   1 => add")
+        print("   2 => spend")
+        print("   3 => balance")
+        cmd = int(input())
+        return cmd
+
+    except Exception as e:
+        print("ERROR")
+        print("ERROR: invalid input")
+        print("ERROR")
+        return -1
+    
+    
+def add(baseurl):
+    url = baseurl + "/add"
+    
+    print("Enter payer name:")
+    payer = input().strip()
+
+    print("Enter amount to add:")
+    points = int(input().strip())
+    
+    time = datetime.now(timezone.utc)
+    timestamp = time.strftime('%Y-%m-%dT%H:%M:%SZ')
+    
+    data = {
+        "payer":payer,
+        "points": points,
+        "timestamp": timestamp
+    }
+    
+    response = requests.post(url, json = data)
+    print(f"Response received with status {response.status_code}")
+    
+    if response.status_code == 400:
+        body = response.json()
+        print(f"Error: {body['error']}")
+
+    
+def spend(baseurl):
+    url = baseurl + "/spend"
+    
+    print("Points to spend:")
+    points = int(input().strip())
+    
+    data = {"points": points}
+    response = requests.post(url, json = data)
+
+    print(f"Response received with status {response.status_code}")  
+    if response.status_code == 400:
+        body = response.json()
+        print(f"Error: {body['error']}")
+    elif response.status_code == 200: 
+        body = response.json()
+        for record in body:
+            print(f"Payer: {record['payer']}, Points: {record['points']}")
+    
+
+def balance(baseurl):
+    url = baseurl + "/balance"
+    response = requests.get(url)
+    print(f"Response received with status {response.status_code}")
+    
+    if response.status_code == 400:
+        body = response.json()
+        print(f"Error: {body['error']}")
+    elif response.status_code == 200:
+        body = response.json()
+        for key, val in body.items():
+            print(f"Payer: {key}, Points: {val}")
+
+        
+
+cmd = prompt()
+
+while cmd != 0:
+    if cmd == 1:
+        add(baseurl)
+    elif cmd == 2:
+        spend(baseurl)
+    elif cmd == 3:
+        balance(baseurl)
+    else:
+        print("Unknown command.")
+        
+    cmd = prompt()
+
+    
---
+++ server.py
@@ -0,0 +1,77 @@
+import pandas as pd
+from flask import Flask, request
+from flask_restful import Resource, Api
+
+app = Flask(__name__)
+api = Api(app)
+datafile = "points_sheet.csv"
+df = pd.read_csv(datafile)
+
+class Add(Resource):
+    def post(self):
+        try:
+            global df
+            data = request.get_json()
+            data = {
+                "payer": [data["payer"]],
+                "points": [data["points"]],
+                "timestamp": [data["timestamp"]]
+            }
+            data = pd.DataFrame(data)
+            
+            df = pd.concat([df, data], ignore_index=True)      
+            df.to_csv(datafile, index = False)
+            return 200            
+        
+        except Exception as e:
+            return {'error': str(e)}, 400
+    
+class Spend(Resource):
+    def post(self):
+        try:
+            global df
+            data = request.get_json()
+            df_sort = df.sort_values(by='timestamp')
+            
+            payers = {}
+            spend_amount = data["points"]
+            for record in df_sort.itertuples():
+                #get amount to deduct
+                payer_amount = int(record.points)
+                amount = min(payer_amount, spend_amount)
+                spend_amount -= amount
+                
+                payers[record.payer] = payers.get(record.payer, 0) - amount
+                df_sort.at[record.Index, 'points'] -= amount 
+                
+                if spend_amount == 0: #done spending
+                    res = [{"payer": key, "points": val} for key,val in payers.items()]
+                    df = df_sort
+                    df_sort.to_csv(datafile, index=False)
+                    return res, 200
+                
+            #if reached here, not enough points 
+            return {'error': "You don't have enough points!"}, 400
+                        
+        except Exception as e:
+            return {'error': str(e)}, 400            
+
+class Balance(Resource):
+    def get(self):   
+        try:
+            global df       
+            res = {}
+            for record in df.itertuples():
+                res[record.payer] = res.get(record.payer, 0) + record.points
+            return res, 200
+    
+        except Exception as e:
+            return {'error': str(e)}, 400
+    
+# attach resource to URL path
+api.add_resource(Add, '/add') 
+api.add_resource(Spend, '/spend') 
+api.add_resource(Balance, '/balance') 
+
+if __name__ == '__main__': 
+    app.run(host = "localhost", port = 8000) 
